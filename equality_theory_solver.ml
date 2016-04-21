5ecccf6775924ea1cc2753462faed5f759df8bf4
Rémi Jézéquel <remjez@users.noreply.github.com>
2016-04-21 22:05:30 +0200

    Closing by congruence of the generated relation.

--- equality_theory_solver.ml
+++ equality_theory_solver.ml
@@ -13,6 +13,8 @@ end
 
 module Tmap = Map.Make(Ordered_term)
 
+module PUF = Persistent_union_find
+
 type memo = {code : int Tmap.t; 
              next_code : int;
              congruence : Persistent_union_find.t}
@@ -46,15 +48,36 @@ let get_code_term memo t =
     memo.next_code, set_code (set_next_code memo (memo.next_code + 1)) 
                              new_code
 
+let rec close_by_congruence memo =
+  let is_congr t1 t2 =
+    match t1,t2 with
+    | Const s1, Const s2 -> s1 = s2
+    | Fun (f1,arg1), Fun (f2,arg2) ->
+        f1 = f2 && List.for_all (fun (tt1,tt2) -> 
+                                   let tt1_code,_ = get_code_term memo tt1
+                                   and tt2_code,_ = get_code_term memo tt2 in
+                                   PUF.find memo.congruence tt1_code = PUF.find memo.congruence tt2_code)
+                                (List.combine arg1 arg2)
+    | _,_ -> false
+  in
+  let new_rel = Tmap.fold (fun t1 c1 rel1 -> 
+                             Tmap.fold (fun t2 c2 rel2 -> if is_congr t1 t2 
+                                                          then PUF.union rel2 c1 c2 
+                                                          else rel2) memo.code rel1)
+                          memo.code memo.congruence
+  in
+  if memo.congruence = new_rel then new_rel
+  else close_by_congruence (set_congruence memo new_rel)
+
 let rec gen_congruence memo l_eq =
   match l_eq with
-  | [] -> memo
+  | [] -> close_by_congruence memo
   | (x,y)::ls ->
       let x_code,memo' = get_code_term memo x in
       let y_code,memo'' = get_code_term memo' y in
-      if Persistent_union_find.find memo.congruence x_code <> Persistent_union_find.find memo.congruence y_code
+      if PUF.find memo.congruence x_code <> PUF.find memo.congruence y_code
       then gen_congruence (set_congruence memo''
-                             (Persistent_union_find.union memo.congruence x_code y_code)) 
+                             (PUF.union memo.congruence x_code y_code)) 
                           ls
       else gen_congruence memo'' ls
 

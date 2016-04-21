f3be6799843da610fb1d8496c4e27ff8b53aadf3
Rémi Jézéquel <remjez@users.noreply.github.com>
2016-04-21 16:58:19 +0200

    Adding of the persistant_array module.

---
+++ persistant_array.ml
@@ -0,0 +1,44 @@
+(* Module taken from the work of Sylvain Conchon 
+ *                            and Jean-Christophe Filliatre *)
+
+type 'a t = 'a data ref
+and 'a data =
+  | Arr of int array
+  | Diff of int * 'a * 'a t
+  | Invalid
+
+let init n f = ref (Arr (Array.init n f))
+
+let rec reroot t = match !t with
+  | Arr _ -> ()
+  | Diff (i, v, t') ->
+    reroot t';
+    begin match !t' with
+    | Arr a as n ->
+      a.(i) <- v;
+      t := n;
+      t' := Invalid
+    | Diff _ | Invalid -> assert false
+    end
+    | Invalid -> assert false
+
+let set t i v =
+  reroot t;
+  match !t with
+  | Arr a as n ->
+      let old = a.(i) in
+      a.(i) <- v;
+      let res = ref n in
+      t := Diff (i, old, res);
+      res
+  | Diff _ | Invalid -> assert false
+
+let rec get t i = match !t with
+  | Arr a -> a.(i)
+  | Diff _ ->
+    reroot t;
+    begin match !t with
+    | Arr a -> a.(i)
+    | Diff _ | Invalid -> assert false
+    end
+  | Invalid -> assert false
